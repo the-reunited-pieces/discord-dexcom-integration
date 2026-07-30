7108f6ee32a18831ba874b6630a0e14d4bc0c853
Koiya.App <1041901@hr.nl>
2026-07-29 23:57:23 -0400

    Initialize discord bot

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-console.log("Hello via Bun!");
+import { Client, Events } from "discord.js";
+
+const client = new Client({ intents: 0 });
+
+const promise = new Promise<Client<true>>((res) => client.on(Events.ClientReady, res));
+await client.login(Bun.env.DISCORD_TOKEN);
+const bot = await promise;
+
+console.log(`Bot started as ${bot.user.tag}`);
